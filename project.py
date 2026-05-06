bab74ff797082b2951e07a0ebe82b7d897fce4bf
juliaisabeltiu831-creator <juliaisabeltiu831@gmail.com>
2026-05-07 06:52:43 +0800

    Update project.py

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ from pyscript import document
 # IMAGE DATA (easy to edit)
 images = [
     {"src": "images/pic1.jpg", "caption": "First Day of School"},
-    {"src": "images/pic2.jpg", "caption": "Class Activity"},
+    {"src": "images/pic2.jpg", "caption": "Intramurals"},
     {"src": "images/pic3.jpg", "caption": "Group Photo"},
     {"src": "images/pic4.jpg", "caption": "School Event"},
 ]
